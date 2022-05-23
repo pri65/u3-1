b673425e2f955bdea7105c6b89b7ef1bf703b4b5
pri65 <priyankasoniii65@gmail.com>
2022-05-23 16:27:00 +0530

    all done

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -1,44 +1,47 @@
 function append(){
     let data=JSON.parse(localStorage.getItem("products")) || [];
     let all=document.getElementById("all_products");
-    all.innerHTML=null;
+    //all.innerHTML=null;
 
    data.forEach(function(el,index){
        let div=document.createElement("div");
        let img=document.createElement("img");
        img.src=el.image;
-       let t=document.createElement("t");
+       let t=document.createElement("p");
        t.innerText=el.type;
-       let d=document.createElement("d");
+       let d=document.createElement("p");
        d.innerText=el.desc;
        let p=document.createElement("p");
        p.innerText=el.price;
-    //  let btn=document.createElement("button");
-    //   btn.innerText=remove;
-    //    btn.setAttribute("onclick",function(){
-    //        remove(index);
-      // });
+     let btn=document.createElement("button");
+      btn.innerText="Remove Product";
+       btn.addEventListener("click",function(){
+           remove(index);
+      });
        
-div.append(img,t,p,d);
+div.append(img,t,p,d,btn);
 all.append(div);
   });
 }
 
 append();
 
-// function remove(index){
-//     let data=JSON.parse(localStorage.getItem("products")) || [];
-//     let newData=data.filter(function(el,i){
-//         if(i===index){
-//             let trash=JSON.parse(localStorage.getItem("trash")) || [];
-//             trash.push(el);
-//             localStorage.setItem("trash",JSON.stringify(trash));
-//         } else {
-//             return i !== index;
-//         }
-//     });
+function remove(index){
+    // console.log(index);
+    let data=JSON.parse(localStorage.getItem("products")) || [];
+    let newData=data.filter(function(el,i){
+      return  i!=index;
+    //     if(i===index){
+    //         let trash=JSON.parse(localStorage.getItem("trash")) || [];
+    //         trash.push(el);
+    //         localStorage.setItem("trash",JSON.stringify(trash));
+    //     } else {
+    //         return i !== index;
+    //     }
 
-//     localStorage.setItem("products",JSON.stringify(newData));
-//     append();
-//     console.log(newData);
-// }
+    });
+    console.log(newData);
+    localStorage.setItem("products",JSON.stringify(newData));
+    //append();
+    location.reload()
+}
